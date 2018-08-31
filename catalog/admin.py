ed009a07050b89d4a317e3e291604a1805e2250a
b3n3b0 <jags4real25@gmail.com>
2018-08-31 13:15:28 +0100

    Register:
    registering the models with django default admin"

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
