0010fe8952a151f40ad390354276b5c5b196d639
b3n3b0 <jags4real25@gmail.com>
2018-08-31 11:49:12 +0100

    INIT
    first commit , the django skeleton and some models are in place"

---
+++ catalog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.urls import reverse
+# Create your models here.
+
+#models representing a book genre
+class Genre(models.Model):
+	name_of_genre = models.CharField(max_length=200, help_text='Enter a book genre(e.g. Science Fiction)')
+
+def __str__(self):
+		return self.name_of_genre
+
+
+#model representing a book
+class Book(models.Model):
+	title = models.CharField(max_length=200)
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+	summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+	isbn = models.ChaarField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+	#many to many fields becos genre can contain many books and books can cover many genres
+	genre = models.ManyToManyField(Genre, help_text = 'Select a genre for this book')
+
+	def __str__(self):
+		return self.title
+	
+	def get_absolute_url(self):
+		return reverse('book-detail', args= [str(self.id)])
