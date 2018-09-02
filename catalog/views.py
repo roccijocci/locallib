52014ea96ab3507daf86e9ba73296d892ac22331
b3n3b0 <jags4real25@gmail.com>
2018-09-02 20:32:57 +0100

    Url mappers| views:
    working on url mappers and views\

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from catalog.models import Book, Author , BookInstance, Genre
 
 # Create your views here.
+def index(request):
+	# view function for homepage of site
+	
+	#Generate counts of some of the main objects
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+	
+	#num AvailableBooks (status = 'a')
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+	
+	num_authors = Author.objects.count()
+
+	context = {
+		'num_books': num_books,
+		'num_instances': num_instances,
+		'num_instances_available': num_instances_available,
+		'num_authors': num_authors,
+	}
+
+	return render(request,'index.html',context=context)
