dfd76fc48b4a8f498dcea84192ea932d312ceb34
b3n3b0 <jags4real25@gmail.com>
2018-10-16 22:41:10 +0100

    üsing groups

--- catalog/models.py
+++ catalog/models.py
@@ -67,6 +67,7 @@ class BookInstance(models.Model):
 	)
 	class Meta:
 		ordering = ['due_back']
+		permissions = (("can_mark_returned","Set book as returned"),)
 
 	def __str__(self):
 		#string for representing the Model object
